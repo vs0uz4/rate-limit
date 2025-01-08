b9cbd963864772fd87da80fe42c44c0717143d20
vs0uz4 <vitor.rodrigues@gmail.com>
2025-01-08 00:14:04 -0300

    add persistence initialize

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/vs0uz4/rate-limit/config"
+	"github.com/vs0uz4/rate-limit/internal/infra/redis"
+	"github.com/vs0uz4/rate-limit/internal/rate_limiter"
 	"github.com/vs0uz4/rate-limit/internal/webserver"
 )
 
@@ -13,7 +16,20 @@ func main() {
 		log.Fatalf("Failed to load configuration: %v", err)
 	}
 
-	if err := webserver.Start(cfg); err != nil {
+	redisClient, err := redis.NewRedisClient(cfg.RedisHost, cfg.RedisPort)
+	if err != nil {
+		log.Fatalf("Failed to initialize Redis: %v", err)
+	}
+
+	redisAdapter := redis.NewRedisAdapter(redisClient.Client)
+
+	limiter := rate_limiter.NewRateLimiter(rate_limiter.Config{
+		Limit:         cfg.LimiterIPLimit,
+		BlockDuration: time.Duration(cfg.BlockDuration) * time.Second,
+		TokenLimits:   cfg.TokenLimits,
+	}, redisAdapter)
+
+	if err := webserver.Start(cfg, limiter); err != nil {
 		log.Fatalf("Failed to start web server: %v", err)
 	}
 }
